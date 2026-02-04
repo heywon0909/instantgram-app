7a4bf7579c763d9ab5f27e01a5e1e0ec0011f486
heywon0909 <hw731911@gmail.com>
2026-02-04 16:06:44 +0900

    feat: color button props 속성 추가

--- src/components/ui/ColorButton.tsx
+++ src/components/ui/ColorButton.tsx
@@ -1,13 +1,17 @@
 type Props = {
   text: string;
-  onClick: () => void;
+  onClick: (...args: unknown[]) => void;
+  size?: "small" | "big";
 };
-export default function ColorButton({ text, onClick }: Props) {
+export default function ColorButton({ text, onClick, size = "small" }: Props) {
   return (
-    <div className="rounded-md bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300 p-[0.2rem]">
+    <div
+      className={`rounded-md bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300 p-[0.2rem] ${size === "big" ? "p-[0.3rem]" : "p-[0.15rem]"}`}
+    >
       <button
         onClick={onClick}
-        className="bg-white rounded-sm text-base p-[0.3rem] hover:opacity-95 transition-opacity"
+        className={`bg-white rounded-sm text-base p-[0.3rem] hover:opacity-95 transition-opacity 
+          ${size === "big" ? "p-4 text-2xl" : "p-[0.3rem] text-base"}`}
       >
         {text}
       </button>
