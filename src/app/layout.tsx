ba45e086341741e229ccf5d818a47f39989915e3
heywon0909 <hw731911@gmail.com>
2026-02-13 17:00:09 +0900

    feat: root layout 및 home page 비동기 로직 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Roboto } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import AuthContext from "../context/AuthContext";
+import SWRConfigConext from "../context/SWRConfigContext";
 
 const roboto = Roboto({
   subsets: ["latin"],
@@ -17,13 +18,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`w-full max-w-screen-xl overflow-auto mx-auto ${roboto.className}`}
+        className={`w-full max-w-screen-xl overflow-auto mx-auto ${roboto.className} flex flex-col`}
       >
         <AuthContext>
           <header className="sticky top-0 bg-white z-10 border-b">
             <Navbar />
           </header>
-          <main className="grow">{children}</main>
+          <main className="w-full flex justify-center bg-neutral-50 grow">
+            <SWRConfigConext>{children}</SWRConfigConext>
+          </main>
         </AuthContext>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,27 @@
-import Stories from '../components/Stories';
-import PostCard from '../components/PostCard';
-import Info from '../components/Info';
-// import { POSTS_QUERY } from '../service/sanity/api';
-// import { client } from '../service/sanity/client';
+import SideBar from "../components/SideBar";
+import FollowingBar from "../components/FollowingBar";
+import PostList from "../components/PostLIst";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
-export default async function Home() {
-  // const posts = await client.fetch(POSTS_QUERY);
-  // console.log('posts:', posts);
+export default async function HomePage() {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  if (!user) {
+    redirect("/auth/signin");
+  }
 
   return (
-    <div className="flex">
-      <div>
-        <Stories />
-        <section className="flex flex-col gap-4">
-          {/* {posts.map(post => (
-            <PostCard {...post} key={post._id} />
-          ))} */}
-        </section>
+    <section className="w-full flex flex-col md:flex-row max-w-[850px] p-4 ">
+      <div className="w-full basis-3/4 min-w-0">
+        <FollowingBar />
+        <PostList />
       </div>
-      <Info />
-    </div>
+      <div className="basis-1/4 ml-8">
+        <SideBar user={user} />
+      </div>
+    </section>
   );
 }
