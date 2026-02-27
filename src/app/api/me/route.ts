15daf60ce309288f859a71b597ed0a7be68376cf
heywon0909 <hw731911@gmail.com>
2026-02-27 13:56:26 +0900

    feat: console.log 제거

--- src/app/api/me/route.ts
+++ src/app/api/me/route.ts
@@ -6,13 +6,11 @@ import { authOptions } from "../auth/[...nextauth]/route";
 export async function GET() {
   const session = await getServerSession(authOptions);
   const user = session?.user;
-  console.log("user", user);
 
   if (!user) {
     return new Response("Authentication Error", { status: 401 });
   }
   return getUserByUsername(user.username).then((data) => {
-    console.log("에잇", data);
     return NextResponse.json(data);
   });
 }
